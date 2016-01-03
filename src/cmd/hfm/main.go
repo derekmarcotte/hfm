6a363e8a592e5a0daa60e5d5831daeac6e8eb606
Derek Marcotte <554b8425@razorfever.net>
2016-01-02 23:34:42 -0500

    add proper program name for syslog

--- src/cmd/hfm/main.go
+++ src/cmd/hfm/main.go
@@ -31,8 +31,12 @@ package main
 /* stdlib includes */
 import (
 	"flag"
+	"fmt"
+	"log/syslog"
 	"os"
+	"path"
 	"runtime"
+	"strings"
 	"time"
 )
 
@@ -41,19 +45,79 @@ import "github.com/op/go-logging"
 
 /* definitions */
 
+type LogConfiguration struct {
+	Where    string
+	Facility string
+}
+
 /* meat */
 /* dependancy injection is for another day */
-var log = logging.MustGetLogger(os.Args[0])
+var log = logging.MustGetLogger(path.Base(os.Args[0]))
+
+func configureLogging(conf LogConfiguration) error {
+	conf.Where = strings.ToLower(conf.Where)
+	switch conf.Where {
+	case "syslog":
+	case "stderr":
+		return nil
+	default:
+		return fmt.Errorf("Invalid log location, must be one of {stderr, syslog}\n")
+	}
+
+	facilityList := map[string]syslog.Priority{
+		"kern":     syslog.LOG_KERN,
+		"user":     syslog.LOG_USER,
+		"mail":     syslog.LOG_MAIL,
+		"daemon":   syslog.LOG_DAEMON,
+		"auth":     syslog.LOG_AUTH,
+		"syslog":   syslog.LOG_SYSLOG,
+		"lpr":      syslog.LOG_LPR,
+		"news":     syslog.LOG_NEWS,
+		"uucp":     syslog.LOG_UUCP,
+		"cron":     syslog.LOG_CRON,
+		"authpriv": syslog.LOG_AUTHPRIV,
+		"ftp":      syslog.LOG_FTP,
+		"local0":   syslog.LOG_LOCAL0,
+		"local1":   syslog.LOG_LOCAL1,
+		"local2":   syslog.LOG_LOCAL2,
+		"local3":   syslog.LOG_LOCAL3,
+		"local4":   syslog.LOG_LOCAL4,
+		"local5":   syslog.LOG_LOCAL5,
+		"local6":   syslog.LOG_LOCAL6,
+		"local7":   syslog.LOG_LOCAL7,
+	}
+
+	conf.Facility = strings.ToLower(conf.Facility)
+
+	f, ok := facilityList[conf.Facility]
+	if !ok {
+		return fmt.Errorf("Invalid syslog facility")
+	}
+
+	be, _ := logging.NewSyslogBackendPriority(path.Base(os.Args[0]), f)
+	log.SetBackend(logging.AddModuleLevel(be))
+
+	return nil
+}
 
 func main() {
 	var configPath string
 	var config Configuration
 
+	var lc LogConfiguration
+
 	flag.StringVar(&configPath, "config", "etc/hfm.conf", "Configuration file path")
+	flag.StringVar(&lc.Where, "log", "stderr", "Where to log {stderr, syslog}")
+	flag.StringVar(&lc.Facility, "facility", "local0", "Log facility (when -log set to syslog) {local0-9, user, etc}")
 	flag.Parse()
 
+	if e := configureLogging(lc); e != nil {
+		fmt.Printf("Could not configure logging: %v", e)
+		panic(e)
+	}
+
 	if e := config.LoadConfiguration(configPath); e != nil {
-		log.Error("Could not load configuration file %v: %+v", configPath, e)
+		fmt.Printf("Could not load configuration file %v: %+v", configPath, e)
 		panic(e)
 	}
 
